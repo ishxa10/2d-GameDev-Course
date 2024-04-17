9bdace5c3542731e87d35ef59b629ba9d52c9480
ixa <irlacayanga@s.sfusd.edu>
2024-04-17 10:58:26 -0700

    L3 to L5

--- 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
+++ 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
@@ -8,9 +8,10 @@ var energy := 20.0
 
 
 func _ready() -> void:
-	get_node("UI/EnergyBar").value = energy
-
-
+	area_entered.connect(_on_area_entered)
+	
+	set_energy(energy)
+	
 func _process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
@@ -23,5 +24,10 @@ func _process(delta: float) -> void:
 		get_node("Sprite2D").rotation = velocity.angle()
 
 
-func _on_area_entered(area: Area2D) -> void:
-	pass
+func set_energy(new_energy: int) -> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
+	
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	set_energy(energy + 20)
+
