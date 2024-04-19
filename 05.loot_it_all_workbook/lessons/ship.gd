4939163c4762289fc27174fef8cb6dda1a98f407
ixa <irlacayanga@s.sfusd.edu>
2024-04-19 14:55:52 -0700

    L6.P2

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -32,6 +32,10 @@ func set_health(new_health: int) -> void:
 	health = new_health
 	get_node("UI/HealthBar").value = health
 
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
+
 func _on_area_entered(area_that_entered: Area2D) -> void:
 	if area_that_entered.is_in_group("gem"):
 		set_gem_count(gem_count + 1)
